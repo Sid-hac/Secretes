1fe95a3989c8f69e1bb72507eb8dafe58e56ce72
sid <sidengineer.014@gmail.com>
2023-08-18 01:57:24 +0530

    update2.7

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(session({
 
