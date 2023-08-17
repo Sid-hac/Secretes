c9999656ea06a034d91750cdb304c3c68a377815
sid <sidengineer.014@gmail.com>
2023-08-18 01:44:35 +0530

    update2.6

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(passport.session());
 
 
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB" , {useNewUrlParser : true , useUnifiedTopology: true});
+mongoose.connect("mongodb://0.0.0.0:27017/userDB" , {useNewUrlParser : true , useUnifiedTopology: true});
 
 const userSchema = new mongoose.Schema({
     email :String,
